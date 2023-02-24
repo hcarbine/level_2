0b33e817873c0be5da3d04f601ede4dccb5a3fac
Holly Carbine <hcarbine@gmail.com>
2023-02-23 17:13:27 -0700

    final commit

--- go_speed_clicker/main.js
+++ go_speed_clicker/main.js
@@ -1,16 +1,22 @@
-var count1 = 0;
-var count2 = 0;
-        var btn1 = document.getElementById("click1");
-        var btn2 = document.getElementById("click2")
-        var disp1 = document.getElementById("display1");
-        var disp2 = document.getElementById("display2")
-          
-        btn1.onclick = function () {
-            count1++;
-            disp1.innerHTML = count1;
-        }
+var count1 = parseInt(localStorage.getItem('count1')) || 0;
+var count2 = parseInt(localStorage.getItem('count2')) || 0;
 
-        btn2.onclick = function () {
-            count2++;
-            disp2.innerHTML = count2;   
-        }
+var btn1 = document.getElementById("click1");
+var btn2 = document.getElementById("click2");
+var disp1 = document.getElementById("display1");
+var disp2 = document.getElementById("display2");
+
+disp1.innerHTML = count1;
+disp2.innerHTML = count2;
+
+btn1.onclick = function () {
+  count1++;
+  disp1.innerHTML = count1;
+  localStorage.setItem('count1', count1);
+};
+
+btn2.onclick = function () {
+  count2++;
+  disp2.innerHTML = count2;
+  localStorage.setItem('count2', count2);
+};
